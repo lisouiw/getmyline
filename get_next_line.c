67b0015e9bd3b5f8f4d6e42803a14a86d790e4bd
Lisa TRAN <ltran@e3r11p14.42.fr>
2017-01-08 23:55:27 +0100

    BACKUPP :D

--- get_next_line.c
+++ get_next_line.c
@@ -6,21 +6,21 @@
 /*   By: ltran <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/12/13 06:06:29 by ltran             #+#    #+#             */
-/*   Updated: 2016/12/29 02:57:43 by ltran            ###   ########.fr       */
+/*   Updated: 2017/01/08 23:52:25 by ltran            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <unistd.h>
 #include "get_next_line.h"
 
 char	*retour(char *buf, int num, char *line)
 {
 	char	*chr;
+	int		i;
 
 	chr = buf;
 	if (num == 1 && line != NULL)
-	{
-		chr = ft_strdup(ft_strcat(line, chr));
-	}
+		chr = ft_strjoin(line, chr);
 	if (num == 0 || num == 2)
 	{
 		chr = ft_strchr(chr, '\n');
@@ -31,57 +31,47 @@ char	*retour(char *buf, int num, char *line)
 	}
 	if (num == 2)
 		chr = ft_strsub(chr, 1, (ft_strlen(chr) - 1));
+	if (num == 3)
+	{
+		i = ft_strlen(chr);
+		while (i-- > 1)
+			(chr)++;
+	}
+	printf("chr{%s}\n", chr);
 	return (chr);
 }
 
 int		get_next_line(int fd, char **line)
 {
-	int				 i;
-	static int		rd;
-	static char		*chr;
-	static char		buf[BUFF_SIZE];
+	static int		r = 0;
+	static char		*chr = NULL;
+	static char		buf[BUFF_SIZE + 1];
 
-	i = 0;
-	if (!rd)
-		rd = read(fd, buf, BUFF_SIZE);
-	while(rd > 0 && rd != 0)
+	if (!r)
 	{
+		r = read(fd, buf, BUFF_SIZE);
+		buf[r] = '\0';
+		printf("<%s>\n", buf);
+	}
+	while (r > 0)
+	{
+		puts("yo\n");
 		chr = retour(buf, 0, *line);
 		*line = retour(chr, 1, *line);
 		ft_strcpy(buf, retour(buf, 2, chr));
-		if (ft_strlen(chr) != ft_strlen(buf) || (ft_strlen(chr) == rd && rd != BUFF_SIZE))
+		*line = retour(buf, 3, *line);
+		if (ft_strcmp(chr, buf) != 0 || (ft_strlen(chr) == r && r != BUFF_SIZE))
+			return (1);
+		if (ft_strlen(buf) == ft_strlen(chr) || ft_strlen(chr) == r)
+			r = read(fd, buf, BUFF_SIZE);
+		if (r > 0 && ft_strlen(buf) != r)
 		{
-			printf("###line = %s\n", *line);
-			i = ft_strlen(*line);
-			while (i != 0)
-			{
-				i--;
-				(*line)++;
-			}
-			printf("len %zu\n", ft_strlen(*line));
-			printf("^^^line = %s\n", *line);
+			ft_strcpy(buf, ft_strsub(buf, 0, r));
+			*line = retour(*line, 1, buf);
+			r = read(fd, buf, BUFF_SIZE);
 			return (1);
 		}
-		if (ft_strlen(buf) == ft_strlen(chr) || ft_strlen(chr) == rd)
-		{
-			rd = read(fd, buf, BUFF_SIZE);
-			printf("if dc rd = %i\n", rd);
-			if (rd > 0 && ft_strlen(buf) != rd)
-				ft_strncpy(buf, buf, rd);
-		}
 	}
-	return (0);
+	printf("BAM\n");
+	return (-1);
 }
-
-/*int		main(int argc, char **argv)
-{
-	int		fd;
-	char	*line = NULL;
-
-	fd = open(argv[1], O_RDWR);
-	while (get_next_line != 0)
-	{
-		get_next_line(fd, &line);
-	}
-	return (0);
-}*/
--- get_next_line.h
+++ get_next_line.h
@@ -6,18 +6,20 @@
 /*   By: ltran <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/12/13 06:06:19 by ltran             #+#    #+#             */
-/*   Updated: 2016/12/26 23:52:09 by ltran            ###   ########.fr       */
+/*   Updated: 2017/01/08 23:50:21 by ltran            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-#define BUFF_SIZE 4
+# include "./../libft/libft.h"
+# include <fcntl.h>
+# include <unistd.h>
+# include <stdio.h>
 
-#include "./../libft/libft.h"
-#include <fcntl.h>
-#include <unistd.h>
-#include <stdio.h>
+# define BUFF_SIZE 9999
+
+int	get_next_line(int fd, char **line);
 
 #endif

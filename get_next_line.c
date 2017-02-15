a7c7b8ecb1e3923a0823be9374f904ca61da564e
Lisa TRAN <ltran@e1r6p19.42.fr>
2017-02-15 06:28:35 +0100

    leaks rst

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: ltran <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/18 01:11:43 by ltran             #+#    #+#             */
-/*   Updated: 2017/01/19 15:15:30 by ltran            ###   ########.fr       */
+/*   Updated: 2017/02/15 06:27:47 by ltran            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,29 +14,31 @@
 
 char	*ft_no(int fd, char *buf, int num, int r)
 {
-	int		i;
 	char	*lg;
-	char	*sub;
 
 	lg = ft_strdup(buf);
-	while (num == 0 && r > 0  && ft_strchr(buf, '\n') == NULL) 
+	while (num == 0 && r > 0 && ft_strchr(buf, '\n') == NULL)
 	{
 		r = ft_read(fd, buf, 0);
 		if (ft_strchr(buf, '\n') == NULL)
 			lg = ft_strjoin(lg, buf);
 	}
-	if (r < BUFF_SIZE  && ft_strchr(buf, '\n') == NULL)
-	{
-		free(lg);
+	if (num == 0 && r < BUFF_SIZE && ft_strchr(buf, '\n') == NULL)
 		return (lg);
-	}
-	i = ft_strlen(buf) - ft_strlen(ft_strchr(buf, '\n'));
-	lg = ft_strjoin(lg, ft_strsub(buf, 0, i));
-	while (num == 1 && i-- >= 0)
+	r = ft_strlen(buf) - ft_strlen(ft_strchr(buf, '\n'));
+	lg = ft_strjoin(lg, ft_strsub(buf, 0, r));
+	while (num == 1 && r-- >= 0)
 		++buf;
 	if (num == 1)
 		return (buf);
-	free(lg);
+	return (lg);
+}
+
+char	*ft_sep(char *buf, size_t b, size_t c)
+{
+	char	*lg;
+
+	lg = ft_strsub(buf, 0, (b - c));
 	return (lg);
 }
 
@@ -54,27 +56,24 @@ int		get_next_line(int fd, char **line)
 	if (ft_strlen(buf) > 0)
 	{
 		ft_strcpy(buf, ft_no(fd, buf, 1, 0));
+		*line = ft_strnew(0);
+		free(*line);
 		if (ft_strlen(buf) == 0 && ft_read(fd, buf, 0) == 0)
-		{
-			*line = ft_strnew(0);
-			free(*line);
 			return (0);
-		}
 		if (ft_strchr(buf, '\n') == buf)
-		{
-			*line = ft_strnew(0);
-			free(*line);
 			return (1);
-		}
 	}
-	if (!(ft_strlen(buf) > 0))
-		ft_read(fd, buf, 0);
+	if (!(ft_strlen(buf) > 0) && ft_read(fd, buf, 0) < 0)
+		return (-1);
+	if (ft_strchr(buf, '\n') != NULL)
+	{
+		*line = ft_sep(buf, ft_strlen(buf), ft_strlen(ft_strchr(buf, '\n')));
+		return (1);
+	}
 	while (ft_strlen(buf) > 0)
 	{
 		*line = ft_no(fd, buf, 0, ft_strlen(buf));
 		return (1);
 	}
-	*line = ft_strnew(0);
-	free(*line);
 	return (0);
 }
